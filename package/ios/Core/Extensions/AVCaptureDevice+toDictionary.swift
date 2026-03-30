929c4138ff1e12798b1e107791d33c7e46ba47de
ralph1233 <ralphmerhy93@gmail.com>
2026-03-30 18:21:35 +0300

    feat: add supportsTemperatureAndTint property to device

--- package/ios/Core/Extensions/AVCaptureDevice+toDictionary.swift
+++ package/ios/Core/Extensions/AVCaptureDevice+toDictionary.swift
@@ -29,6 +29,7 @@ extension AVCaptureDevice {
       "supportsRawCapture": false, // TODO: supportsRawCapture
       "supportsLowLightBoost": isLowLightBoostSupported,
       "supportsFocus": isFocusPointOfInterestSupported,
+      "supportsTemperatureAndTint": isLockingWhiteBalanceWithCustomDeviceGainsSupported,
       "hardwareLevel": HardwareLevel.full.jsValue,
       "sensorOrientation": sensorOrientation.jsValue,
       "formats": formats.map { $0.toJSValue() },
